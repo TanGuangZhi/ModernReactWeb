3bb10f1349dcc364a565e6c604c9e258e051882e
tanguangzhi <tanguangzhi@forensix.cn>
2022-09-21 16:34:08 +0800

    refactor(App.js): some new component

--- oh-my-kanban/src/App.js
+++ oh-my-kanban/src/App.js
@@ -1,54 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState } from "react";
-import { css } from "@emotion/react";
 import logo from "./logo.svg";
 import "./App.css";
 import KanbanBoard from "./KanbanBoard";
 import { KanbanCard } from "./KanbanCard";
 import { KanbanNewCard } from "./KanbanNewCard";
+import { KanbanColumn } from "./KanbanColumn";
 
-const KanbanColumn = ({ children, bgColor, title }) => {
-  return (
-    <section
-      css={css`
-        flex: 1 1;
-        display: flex;
-        flex-direction: column;
-        border: 1px solid gray;
-        border-radius: 1rem;
-        background-color: ${bgColor};
-
-        & > h2 {
-          margin: 0.6rem 1rem;
-          padding-bottom: 0.6rem;
-          border-bottom: 1px solid gray;
-
-          & > button {
-            float: right;
-            margin-top: 0.2rem;
-            padding: 0.2rem 0.5rem;
-            border: 0;
-            border-radius: 1rem;
-            height: 1.8rem;
-            line-height: 1rem;
-            font-size: 1rem;
-          }
-        }
-
-        & > ul {
-          flex: 1;
-          flex-basis: 0;
-          margin: 1rem;
-          padding: 0;
-          overflow: auto;
-        }
-      `}
-    >
-      <h2>{title}</h2>
-      <ul>{children}</ul>
-    </section>
-  );
-};
 const COLUMN_BG_COLORS = {
   todo: "#C9AF97",
   ongoing: "#FFE799",
